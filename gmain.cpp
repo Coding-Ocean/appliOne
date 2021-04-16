4f2fc6b0c6231a0a22518e9749abdd3ff0d628f2
Coding-Ocean <eijo524@gmail.com>
2021-04-16 14:31:20 +0900

    delete graphics

--- gmain.cpp
+++ gmain.cpp
@@ -1,19 +1,7 @@
 #include"libOne.h"
 void gmain() {
-    window(800, 800);
-    clear(220);
-    float v = 0;
-    while (notQuit)
-    {
-        v += 10;
-        if (v>1600) {
-            fill(220,220,220,5);
-            rect(0, 0, width, height);
-            if (v > 3200) {
-                clear(220);
-                v = 0;
-            }
-        }
-        circle(v, height/2, v);
+    window(1280, 720);
+    clear(220,220,220);
+    while (notQuit){ 
     }
 }
